8fb2de743bb9faf54c185623b8312d5520b4f07e
delano <delano.mandelbaum@communitysift.com>
2015-11-22 11:41:48 -0800

    Store latest slideshow and view pulls from it

--- index.js
+++ index.js
@@ -12,42 +12,7 @@ app.set('views', __dirname + '/views');         // HTML templates (not used)
 app.set('view engine', 'ejs');                  // Template type (not used)
 // ------------ END CONFIGURATION ----------
 
-var DEFAULT_SLIDESHOW = {
-  // REPLACE WITH HARDCODED JSON SLIDESHOW
-};
-
-var uriLogic = {
-  /*
-    Create a new slide show
-  */
-  runSlideshow: function(request, response) {
-    var entropy = ['slideshow', Math.random()];
-    var slideshowId = helperFunction.createSHA1(entropy);
-    var keyDefinition = ['careageous', 'slideshow', slideshowId];
-    var key = helperFunction.createRedisKey(keyDefinition);
-    var slideshow = DEFAULT_SLIDESHOW;
-    slideshow.sid = slideshowId;
-    var callback = function(err, result) {
-    if (err) return response.json({status: false, err: err});
-      response.json({status: true, slideshow: slideshow});
-    };
-    client.set(key, JSON.stringify(slideshow), callback);
-
-  },
-  /*
-    View most recent slideshow
-  */
-  viewSlideshow: function somefunction(request, response) {
-    var callback = function(err, value) {
-      if (err) return response.json({status: false, err: err});
-      var json = JSON.parse(value);
-      response.json({status: true, slideshow: json});
-    };
-    var key = 'key:test';
-    client.get(key, callback);
-  }
-};
-
+// -------------- START HELPERS ------------
 var helperFunction = {
   createRedisKey: function(values) {
     var key = values.join(':');
@@ -57,6 +22,7 @@ var helperFunction = {
   createSHA1: function(entropy) {
     var current_date = (new Date()).valueOf().toString();
     var random = Math.random().toString();
+    entropy.push('v1');
     entropy.push(current_date);
     entropy.push(random);
     var inputStr = entropy.join(':');
@@ -64,6 +30,52 @@ var helperFunction = {
     return crypto.createHash('sha1').update(inputStr).digest('hex');
   }
 }
+// ---------------- END HELPERS -------------
+
+
+var DEFAULT_SLIDESHOW = {
+  // REPLACE WITH HARDCODED JSON SLIDESHOW
+};
+var KEY_SLIDESHOW = helperFunction.createRedisKey(['careageous', 'slideshows', 'latest']);
+
+var uriLogic = {
+  /*
+    Create a new slide show (work in progress)
+  */
+  runSlideshow: function(request, response) {
+    var entropy = ['slideshow', Math.random()];
+    var slideshowId = helperFunction.createSHA1(entropy);
+    var keyDefinition = ['careageous', 'slideshow', slideshowId];
+    var key = helperFunction.createRedisKey(keyDefinition);
+    var slideshow = DEFAULT_SLIDESHOW;
+    slideshow.sid = slideshowId;
+    var callback = function(err, result) {
+      if (err) return response.json({status: false, err: err});
+      client.set(KEY_SLIDESHOW, slideshowId);
+      response.json({status: true, slideshow: slideshow});
+    };
+    client.set(key, JSON.stringify(slideshow), callback);
+  },
+  /*
+    View most recent slideshow (work in progress)
+  */
+  viewSlideshow: function somefunction(request, response) {
+    var getSlideshow = function(err, value) {
+      if (err) return response.json({status: false, err: err});
+      var json = JSON.parse(value);
+      response.json({status: true, slideshow: json});
+    };
+    var getLatestSlideshowKey = function(err, slideshowId) {
+      if (err) return response.json({status: false, err: err});
+      var keyDefinition = ['careageous', 'slideshow', slideshowId];
+      var key = helperFunction.createRedisKey(keyDefinition);
+      client.get(key, getSlideshow);
+    };
+    var key = KEY_SLIDESHOW;
+    client.get(key, getLatestSlideshowKey);
+  }
+};
+
 
 // ------- START ROUTE CONFIGURATION --------
 app.get ('/slideshow', uriLogic.runSlideshow);
