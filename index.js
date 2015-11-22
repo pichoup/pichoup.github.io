f70f7990d6b1854a69c31fece046d0e04df32cb7
delano <delano.mandelbaum@communitysift.com>
2015-11-22 11:13:57 -0800

    Added helper functions and sha1 helper

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var crypto = require('crypto');
 var express = require('express');
 var app = express();
 var redisvar = process.env.REDIS_URL || "redis://h:p4uj069431mqe8a04m1mk6rniqc@ec2-54-83-9-36.compute-1.amazonaws.com:21039";
@@ -12,15 +13,16 @@ app.set('view engine', 'ejs');                  // Template type (not used)
 // ------------ END CONFIGURATION ----------
 
 
-var logic = {
+
+var uriLogic = {
   /*
     Create a new slide show
   */
   runSlideshow: function(request, response) {
     var callback = function(err, value) {
-    if (err) return response.json({status: false, err: err});
-    var json = JSON.parse(value);
-    response.json({status: true, message: "also_good_too", value: value, json: json});
+      if (err) return response.json({status: false, err: err});
+      var json = JSON.parse(value);
+      response.json({status: true, message: "also_good_too", value: value, json: json});
     };
     var key = 'key:test';
     client.get(key, callback);
@@ -29,7 +31,6 @@ var logic = {
     View most recent slideshow
   */
   viewSlideshow: function somefunction(request, response) {
-    //response.render('pages/index');
     var key = 'key:test';
     var value = {value: Math.random()};
     var callback = function(err, result) {
@@ -40,11 +41,18 @@ var logic = {
   }
 };
 
+var helperFunction = {
+  createSHA1: function(entropy) {
+    var current_date = (new Date()).valueOf().toString();
+    var random = Math.random().toString();
+    return crypto.createHash('sha1').update(current_date + random + entropy).digest('hex');
+  }
+}
 
 // ------- START ROUTE CONFIGURATION --------
-app.get ('/slideshow', logic.runSlideshow);
-app.post('/slideshow', logic.runSlideshow);
-app.get ('/grandma',   logic.viewSlideshow);
+app.get ('/slideshow', uriLogic.runSlideshow);
+app.post('/slideshow', uriLogic.runSlideshow);
+app.get ('/grandma',   uriLogic.viewSlideshow);
 // --------- END ROUTE CONFIGURATION --------
 
 
